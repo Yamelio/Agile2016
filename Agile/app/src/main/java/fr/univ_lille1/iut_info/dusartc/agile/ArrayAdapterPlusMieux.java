7b32905800705fbfd53d05678b5a86e6efea920b
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2016-03-24 17:21:20 +0100

    marchepas

--- Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/ArrayAdapterPlusMieux.java
+++ Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/ArrayAdapterPlusMieux.java
@@ -1,10 +1,14 @@
 package fr.univ_lille1.iut_info.dusartc.agile;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by dusartc on 24/03/16.
@@ -15,16 +19,22 @@ public class ArrayAdapterPlusMieux extends ArrayAdapter {
     private String[] subSpinnerString;
 
     public ArrayAdapterPlusMieux(Context context, int resource, int textViewResourceId, String[] subSpinnerString){
-        super(context,resource,textViewResourceId);
+        super(context,resource,textViewResourceId, subSpinnerString);
         this.context=context;
         this.subSpinnerString = subSpinnerString;
     }
 
-    public View getView(int position,View convertView, ViewGroup parent){
-        View rep = super.getView(position, convertView, parent);
-        Spinner subSpinner = (Spinner) rep.findViewById(R.id.subSpinner);
-        subSpinner.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, subSpinnerString));
-        return rep;
+    public View getView(int position, View convertView, ViewGroup parent){
+        Object tab = getItem(position);
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listevoeux, parent, false);
+        }
+        Spinner subSpinner = (Spinner) convertView.findViewById(R.id.spinner);
+        TextView textView = (TextView) convertView.findViewById(R.id.textView_voeux);
+        textView.setText("bonjourlol");
+        //subSpinner.setAdapter(ArrayAdapter.createFromResource(context, 1, android.R.layout.simple_spinner_item));
+        subSpinner.setAdapter(new ArrayAdapter<CharSequence>(context, android.R.layout.simple_spinner_item, subSpinnerString));
+        return convertView;
     }
 
 }
--- Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/ListeVoeuxActivity.java
+++ Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/ListeVoeuxActivity.java
@@ -1,37 +1,55 @@
 package fr.univ_lille1.iut_info.dusartc.agile;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 /**
  * Created by dusartc on 24/03/16.
  */
-public class ListeVoeuxActivity extends Activity {
+public class ListeVoeuxActivity extends ListActivity {
+
+    private int count=0;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.affichage);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+        //ListView listView = (ListView) findViewById(R.id.listView);
 
-        Voeu[] listeVoeux = (Voeu[]) savedInstance.get("listeVoeux");
-        if (listeVoeux == null){
+        /*Voeu[] listeVoeux;
+        try {
+            listeVoeux = (Voeu[]) savedInstance.get("listeVoeux");
+        } catch (Exception e){
             listeVoeux = new Voeu[]{new Voeu("oui","oui","oui","non")};
         }
+
         String[] subSpinnerString = new String[listeVoeux.length + 1];
         subSpinnerString[subSpinnerString.length-1] = "delete";
         for (int i=0; i<subSpinnerString.length; i++){
             subSpinnerString[i] = i+"";
-        }
+        }*/
+        //listView.setAdapter(new ArrayAdapterPlusMieux(this, R.layout.listevoeux
+        //      , R.id.listView_voeux, subSpinnerString));
 
-        listView.setAdapter(new ArrayAdapterPlusMieux(this, R.layout.listevoeux, R.id.listView, subSpinnerString));
+        list = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        setListAdapter(arrayAdapter);
+    }
 
-
+    public void addItem(View view){
+        list.add("oui + "+count++);
+        arrayAdapter.notifyDataSetChanged();
     }
 
 }
