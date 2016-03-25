8512fe0d521eb7d4560824676b38d2c634d4d02c
Benjamin Deregnaucourt <Benjamin.Deregnaucourt@univ-lille1.fr>
2016-03-25 10:36:54 +0100

    modification LoginDisplayActivity.java

--- Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/LoginDisplayActivity.java
+++ Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/LoginDisplayActivity.java
@@ -10,37 +10,65 @@ import android.view.MenuItem;
 
 import android.view.View;
 import  android.content.Intent;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static android.widget.AdapterView.*;
 
 /**
  * Created by deregnab on 24/03/16.
  */
 public class LoginDisplayActivity extends Activity {
-    //final String EXTRA_LOGIN = "login";
+    final String EXTRA_LOGIN = "login";
 
     public void onCreate(Bundle savedInstanceStat){
         super.onCreate(savedInstanceStat);
         setContentView(R.layout.login_display);
-        //final EditText login = (EditText) findViewById(R.id.login);
+        final EditText login = (EditText) findViewById(R.id.login);
+
         //final EditText pass = (EditText) findViewById(R.id.mdp);
         Button loginButton = (Button) findViewById(R.id.buttonConnection);
         loginButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-               goToListeVoeux();
+                Intent intent = new Intent(LoginDisplayActivity.this, AjoutVoeuActivity.class);
+/* Les tutos de KEVIN
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get("http://13.123.123.123:8080/lesgagnant", null, new AsyncHttpResponseHandler() {
+                   @Override
+                   public void onSuccess(String response) {
+                       try {
+                           JSONObject json = new JSONObject(response);
+                           json.get("bite");
+                       } catch (JSONException e) {
+                           e.printStackTrace();
+                       }
+
+                   }
+
+                    @Override
+                    public void onFailure(Throwable e, String response) {
+
+                    }
+                });
+                requestparam data que t'envoies
+                RequestParams params = new RequestParams();
+                params.put("bite", "bite"); => {"bite":"bite"}
+*/
+                intent.putExtra(EXTRA_LOGIN, login.getText().toString());
+                startActivity(intent);
             }
         });
 
 
     }
 
-    public void goToListeVoeux(){
-        //Intent intent = new Intent(LoginDisplayActivity.this, ListeVoeuxActivity.class);
-        Intent intent = new Intent(LoginDisplayActivity.this, AjoutVoeuActivity.class);
 
-        //intent.putExtra(EXTRA_LOGIN, login.getText().toString());
-        startActivity(intent);
-    }
 
 }
