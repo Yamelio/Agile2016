e93dd8a540ea89df04a9b82b4602fdcd0f06ff62
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2016-03-29 17:21:32 +0200

    onarien

--- Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/AjoutVoeuActivity.java
+++ Agile/app/src/main/java/fr/univ_lille1/iut_info/dusartc/agile/AjoutVoeuActivity.java
@@ -1,119 +1,128 @@
 package fr.univ_lille1.iut_info.dusartc.agile;
 
-import android.content.Intent;
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-import java.io.Serializable;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class AjoutVoeuActivity extends AppCompatActivity {
 
-    private Voeu voeu;
-    private ArrayList<Voeu> listvoeux;
+    private ArrayList<Voeu> listVoeux;
+    private ArrayList<String> listDiplome;
+    private ArrayList<String> listFormation;
+    private String server = "http://172.18.48.149:8080/micheline/";
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ajoutvoeu);
+        setContentView(R.layout.ajout);
+        queue = Volley.newRequestQueue(this);
+        listDiplome = getDiplome();
+        final Spinner spinner = (Spinner) findViewById(R.id.ajout_spinner);
+        final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listDiplome);
+        spinner.setAdapter(adapter);
 
-        voeu = (Voeu) getIntent().getSerializableExtra("voeu");
-        recupListeVoeux();
-
-        if (voeu != null) {
-            EditText etablissement = (EditText) findViewById(R.id.editTextEtablissement);
-            etablissement.setText(voeu.getEtablissement().getNom() + " " + voeu.getEtablissement().getVille());
-            etablissement.setFocusable(false);
-            etablissement.setClickable(false);
-            EditText formation = (EditText) findViewById(R.id.editTextFormation);
-            formation.setText(voeu.getFormation().getDiplome() + " " + voeu.getFormation().getDomaine());
-            formation.setFocusable(false);
-            formation.setClickable(false);
-            EditText order = (EditText) findViewById(R.id.editTextOrder);
-            order.setText(voeu.getOrder()+"", TextView.BufferType.EDITABLE);
-            Button delete = (Button) findViewById(R.id.buttonDelete);
-            delete.setVisibility(View.VISIBLE);
-        }
-    }
-
-    public void validerVoeu(View view){
-        EditText etablissement = (EditText) findViewById(R.id.editTextEtablissement);
-        EditText formation = (EditText) findViewById(R.id.editTextFormation);
-        EditText order = (EditText) findViewById(R.id.editTextOrder);
-
-        if (voeu == null){
-            String[] parsed = etablissement.getText().toString().split(" ");
-            Etablissement etablissement1 = new Etablissement(parsed[0], TextUtils.join(" ",parsed)); //TODO sans le premier element
-            parsed = formation.getText().toString().split(" ");
-            Formation formation1 = new Formation(parsed[0], parsed[1]);
-            int order1 = Integer.parseInt(order.getText().toString());
-            if (order1 < 1)
-                order1 = 1;
-            for(int i = order1 - 1; i < listvoeux.size(); i++){
-                Voeu tmp = listvoeux.get(i);
-                tmp.setOrder(i+2);
-            }
-            if (order1 > listvoeux.size()){
-                listvoeux.add(new Voeu(etablissement1, formation1, listvoeux.size() + 1));
-            } else {
-                listvoeux.add(order1, new Voeu(etablissement1, formation1, order1));
-            }
-        } else {
-            int order1 = Integer.parseInt(order.getText().toString());
-            if(listvoeux.size() > 1){
-                Voeu moved = listvoeux.get(order1 - 1);
-                moved.setOrder(voeu.getOrder());
-            }
-            listvoeux.remove(this.voeu);
-            voeu.setOrder(order1);
-            listvoeux.add(voeu);
-        }
-
-        Collections.sort(listvoeux, new Comparator<Voeu>() {
+        final ListView listView = (ListView) findViewById(R.id.ajout_listView);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public int compare(Voeu lhs, Voeu rhs) {
-                return lhs.compareTo(rhs);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = spinner.getSelectedItem().toString();
+                listFormation = getFormation(selected);
+                ArrayAdapter adapter1 = new ArrayAdapter(AjoutVoeuActivity.this, android.R.layout.simple_list_item_1, listFormation);
+                listView.setAdapter(adapter1);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
-
-        goToListeVoeux();
     }
 
-    public void deleteVoeu(View view){
-        int idx = listvoeux.indexOf(voeu);
-        for (int i = idx+1; i < listvoeux.size(); i++){
-            listvoeux.get(i).setOrder(i);
-        }
-        listvoeux.remove(voeu);
-        Collections.sort(listvoeux, new Comparator<Voeu>() {
+    private ArrayList<String> getDiplome() {
+        final ArrayList<String> ans = new ArrayList<>();
+        StringRequest request = new StringRequest(Request.Method.GET, server + "formation/diplome",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String json) {
+                        JSONArray array = null;
+                        try {
+                            array = new JSONArray(json);
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject object = array.getJSONObject(i);
+                                ans.add(object.getString("lib"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        showError(volleyError);
+                    }
+        });
+        queue.add(request);
+        return ans;
+    }
+
+    private ArrayList<String> getFormation(String requete) {
+        final ArrayList<String> ans = new ArrayList<>();
+        StringRequest request = new StringRequest(Request.Method.GET, server + "formation?" + requete,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String json) {
+                        JSONArray array = null;
+                        try {
+                            array = new JSONArray(json);
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject object = array.getJSONObject(i);
+                                ans.add(object.getString("lib"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public int compare(Voeu lhs, Voeu rhs) {
-                return lhs.compareTo(rhs);
+            public void onErrorResponse(VolleyError volleyError) {
+                showError(volleyError);
             }
         });
-
-        goToListeVoeux();
+        queue.add(request);
+        return ans;
     }
 
-    private void goToListeVoeux(){
-        Intent intent = new Intent(this, ListeVoeuxActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("listVoeux", (Serializable) listvoeux);
-        intent.putExtra("bundle", bundle);
-        startActivity(intent);
+    private void showError(VolleyError error){
+
     }
 
-    private void recupListeVoeux(){
-        Intent intent = getIntent();
-        Bundle bundle = intent.getBundleExtra("bundle");
-        listvoeux = (ArrayList<Voeu>) bundle.getSerializable("listVoeux");
+    public void mayonnaise(View view){
+        ListView listView = (ListView) findViewById(R.id.ajout_listView);
+        String selected = listView.getSelectedItem().toString();
+
     }
+
 }
